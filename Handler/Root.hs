2d853fd1618dfb60be2618d40fd1d35aed8506c0
szatkus <szatkus@gmail.com>
2012-01-20 23:41:37 +0100

    Clean

--- Handler/Root.hs
+++ Handler/Root.hs
@@ -1,7 +1,6 @@
 module Handler.Root where
 
 import Import
-import Yesod
 
 -- This is a handler function for the GET request method on the RootR
 -- resource pattern. All of your resource patterns are defined in
@@ -17,5 +16,4 @@ getRootR = do
         setTitle "haffen homepage"
         $(widgetFile "homepage")
         
-postRootR = do
-	defaultLayout $ "fff"
+
